ccb174b03c166ce1936cbe2187a5fc9be48ac711
qazi-humayoon <qazi.humayoon687@gmail.com>
2023-10-26 17:04:38 +0530

    Binary

---
+++ Binary Search/CountOccurences.py
@@ -0,0 +1,65 @@
+
+#In this code we get the first and last occurance and then subtract last from first + 1 which gives us total occurence
+def firstOccurrence(arr, n, k):
+    low = 0
+    high = n - 1
+    first = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] == k:
+            first = mid
+            # look for smaller index on the left
+            high = mid - 1
+        elif arr[mid] < k:
+            low = mid + 1  # look on the right
+        else:
+            high = mid - 1  # look on the left
+
+    return first
+
+
+def lastOccurrence(arr, n, k):
+    low = 0
+    high = n - 1
+    last = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] == k:
+            last = mid
+            # look for larger index on the right
+            low = mid + 1
+        elif arr[mid] < k:
+            low = mid + 1  # look on the right
+        else:
+            high = mid - 1  # look on the left
+
+    return last
+
+#No need for this function we can directly do. It was due to copy paste from first and last position.
+#______________________________________________________________________________________
+
+# def firstAndLastPosition(arr, n, k):
+    # first = firstOccurrence(arr, n, k)
+    # if first == -1:
+    #     return (-1, -1)
+    # last = lastOccurrence(arr, n, k)
+    # return (first, last)
+#______________________________________________________________________________________
+
+def count(arr,n,x):
+    first = firstOccurrence(arr, n, x)
+    if first == -1:
+        return (-1, -1)
+    last = lastOccurrence(arr, n, x)
+    return last - first + 1
+
+
+arr = [2, 4, 6, 8, 8, 8, 11, 13]
+n = 8
+x = 8
+ans = count(arr, n, x)
+print("The number of occurrences is:", ans)
--- Binary Search/FirstandLastPositionofelement.py
+++ Binary Search/FirstandLastPositionofelement.py
@@ -1,1 +1,121 @@
-print("hello how  r u")
+#Brute force  linear search with Tc O(N)
+# def checking(arr,k):
+#     n = len(arr)
+#     first,last = -1,-1
+#     for i in range(n):
+#         if arr[i] == k:
+#             if first == -1:
+#                 first = i
+#             last = i
+#     return first,last
+        
+
+
+# arr = [3,4,13,13,13,20,40]
+# k = 13
+# ans = checking(arr,k)
+# print(ans)
+
+#__________________---------------------______________________________-_--------------------------------________________
+
+#Using lower and upper bound to get first and last position
+# TC : O(2LogN)
+
+# def lb(arr,k):
+#     n = len(arr)
+#     low = 0
+#     high = n - 1
+#     ans = n
+#     while low <= high:
+#         mid = (low + high) // 2
+#         if arr[mid] >= k:
+#             ans = mid
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return ans
+
+# #Upper bound
+
+# def ub(arr,k):
+#     n = len(arr)
+#     low = 0
+#     high = n - 1
+#     ans = n
+#     while low <= high:
+#         mid = (low + high) // 2
+#         if arr[mid] > k:
+#             ans = mid
+#             high = mid - 1
+#         else:
+#             low = mid + 1
+#     return ans
+
+
+# # ans = checking1(arr,k)
+# # print(ans)
+
+# def checking2(arr,k):
+#     n = len(arr)
+#     f = lb(arr,k)
+#     if f == n or arr[f] != k:
+#         return (-1,-1)
+#     return (f,ub(arr,k) - 1)
+# arr = [3,4,13,13,13,20,40]
+# k = 13
+# indexes = checking2(arr,k)
+# print("The Starting and end index of element is :",indexes)
+
+
+#__________________________________________________________________________________________________________________
+
+#Using simple binary search to get first and last position
+# TC : O(2LogN)
+
+def firstoccurance(arr,k):
+    n = len(arr)
+    low = 0
+    high = n - 1
+    first = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == k:
+            first = mid
+            high = mid - 1
+        elif arr[mid] > k:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return first
+
+def lastoccurance(arr,k):
+    n = len(arr)
+    low = 0
+    high = n - 1
+    last = -1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == k:
+            last = mid
+            low = mid + 1
+        elif k > arr[mid]:
+            low = mid = 1
+        else:
+            high = mid - 1
+    return last
+
+
+def firstandlast(arr, k):
+    first = firstoccurance(arr,k)
+    if first == -1:
+        return (-1, -1)
+    last = lastoccurance(arr, k)
+    return (first, last)
+
+
+
+arr = [3,4,13,13,13,20,40]
+k = 13
+
+ans = firstandlast(arr,k)
+print("The first and last index is :",ans)
