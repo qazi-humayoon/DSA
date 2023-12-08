833a751efd6f1ac77f989560ca1d1b7aa9761b90
qazi-humayoon <qazi.humayoon687@gmail.com>
2023-12-08 19:53:26 +0530

    Basics

--- Basics/primeno.py
+++ Basics/primeno.py
@@ -24,6 +24,22 @@ import math
 # n = 14
 # result = funcc(n)
 # print(result)
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+# Get input from the user
+num = int(input("Enter a number: "))
+
+# Check if the number is prime
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
 
 
 
